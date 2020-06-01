dac756e9b2ddc4592c648bdd79ffbac1a2473cc9
Shyam <shyamramanan@gmail.com>
2020-06-02 01:27:28 +0530

    v 2.0 inculded about route

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def main_app():
     return main.generate_heatmap()
 
+@app.route('/about')
+def about():
+    return "This is a python REST application created for educational purpose. Data are fetched from international " \
+           "credible sources inferences and findinds are purely experiments. Not for research purpose. Please contact " \ 
+           "shyamramanan@gmail.com "
+
 @app.route('/ok')
 def health():
     return "ok"
