93ae3d6c42882c0cce4eb2e61a0b0b4c5c197db0
Shyam <shyamramanan@gmail.com>
2020-06-03 23:22:59 +0530

    included descartes in requirement

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, send_file, request
 import main
 
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def main_app():
     return main.generate_results()
+
+
+@app.route('/maps/<map_type>')
+def get_maps(map_type):
+    if request.args.get('type') == '1':
+        filename = map_type + '.jpg'
+    else:
+        filename = 'error.gif'
+    return send_file(filename, mimetype='image/jpg')
+
 
 @app.route('/about')
 def about():
@@ -13,9 +26,11 @@ def about():
            credible sources inferences and findings are purely experiments. Not for research purpose. Please contact \n 
            shyamramanan@gmail.com """
 
+
 @app.route('/ok')
 def health():
     return "ok"
+
 
 if __name__ == '__main__':
     app.run()
